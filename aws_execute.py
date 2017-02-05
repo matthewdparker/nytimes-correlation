d8f033dc9f012fe2f52d86435137138c498e0346
Matt Parker <matthewdparker@gmail.com>
2017-02-04 18:11:17 -0800

    Updating annotation of aws_execute script

--- aws_execute.py
+++ aws_execute.py
@@ -1,6 +1,12 @@
 from utils2 import create_cooccurrence_dict
 import sys
 
+"""
+Script retrieves cooccurrence counts within NY Times articles for pairs of companies contained in local .csv file found in local pairsn.csv file, where n is first input after filename.
+
+Sample command line execution: python aws_execute.py 4 ***API Key***
+"""
+
 if __name__ == '__main__':
     n = sys.argv[1]
     api_key = sys.argv[2]
